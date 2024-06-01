1a372a9448a51ccfb00235580aeb6e43de39374e
asfand naveed <asfandcodeHive@gmail.com>
2024-06-01 16:30:53 +0500

    changess

---
+++ php/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$serverAddress = 'localhost';
+$username = 'root';
+$password = '';
+$databaseName = 'corvit_feb17_2024';
+
+$con = new mysqli($serverAddress,$username,$password,$databaseName);
+
+
+
+?>
